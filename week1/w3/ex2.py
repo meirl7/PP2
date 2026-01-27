66a07ce38a525700fb36be81e995f8999fcb05fd
meirl7 <meirlan.me@gmail.com>
2026-01-27 14:30:02 +0500

    a

---
+++ week1/w3/ex2.py
@@ -0,0 +1,24 @@
+a = 237846
+b = 7625366
+c = a * b
+
+print("c =", c)
+print("a =", a)
+
+# this is comment
+"""
+multi line comment
+"""
+
+
+str1 = "AA"
+str2 = "BB"
+
+print(str1 + str2)
+
+print(str2.lower())
+
+print("\n")
+
+line = "jsrfre"
+print(line.center())
---
+++ week1/w3/ex3.py
@@ -0,0 +1,9 @@
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
+a = " Hello, World! "
+print(a.strip())
+
+complex_number = 1j
+x = frozenset({"apple", "banana", "cherry"})
+
+print(complex_number)
