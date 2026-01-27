aa8c7943beac922f0888fe81dd0c326650721928
meirl7 <meirlan.me@gmail.com>
2026-01-27 14:58:34 +0500

    exam

---
+++ week1/w3/ex4.py
@@ -0,0 +1,10 @@
+name = "Meirlan"
+city = "Almaty"
+
+message = "Hello, " + name
+full_message = message + " from " + city
+
+print(full_message)
+print(name.upper())
+print(city.lower())
+print(len(name))
---
+++ week1/w3/ex5.py
@@ -0,0 +1,12 @@
+age = 20
+height = 1.78
+is_student = True
+
+next_age = age + 1
+double_height = height * 2
+
+print(age)
+print(next_age)
+print(height)
+print(double_height)
+print(is_student)
