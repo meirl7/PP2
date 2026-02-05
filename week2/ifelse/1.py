66109f2e092ca9274ac08aa22706a3287439c219
meirl7 <meirlan.me@gmail.com>
2026-02-05 20:52:49 +0500

    if else finished

--- week2/ifelse/1.py
+++ week2/ifelse/1.py
@@ -15,3 +15,43 @@ print(x // y)
 
 print(6 ^ 3)
 print(6 | 3)
+
+
+# IF STATEMENTS
+
+a = True
+
+if a:
+    print("a is true")
+if not not a:
+    print("a is not not true")
+
+
+day = 3
+
+
+"""
+elif
+=
+if condition:
+    commands
+else:
+    if condition_for_elif:
+        command for elif
+
+"""
+
+if day == 1:
+    print("Monday")
+elif day == 2:
+     print("Tuesday")
+elif day == 3:
+     print("Wednesday")
+elif day == 4:
+     print("Thursday")
+elif day == 5:
+    print("Friday")
+elif day == 6:
+     print("Saturday")
+elif day == 7:
+     print("Sunday")
---
+++ week2/ifelse/2.py
@@ -0,0 +1,44 @@
+number = 7
+
+if number % 2 == 0:
+     print("The number is even")
+else:
+    print("The number is odd")
+
+
+temperature = 22
+
+if temperature > 30:
+     print("It's hot outside!")
+elif temperature > 20:
+      print("It's warm outside")
+elif temperature > 10:
+     print("It's cool outside")
+else:
+     print("It's cold outside!")
+
+
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+
+day = 4
+match day:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
+    case _:
+        print("No match")
+
